18d9b6d391aa44093be20ca1680e82e8900fc899
Alexander Blaschko-Schänzer <alexander.blaschko@gmail.com>
2024-04-07 12:23:25 +0200

    Added classnames to the project and made the links more dynamic

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,15 @@
+'use client';
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { BsBugFill } from "react-icons/bs";
+import classnames from "classnames";
 
 const NavBar = () => {
+    const currentPath = usePathname();
+    console.log(currentPath);
+
     const links = [
         {label: 'Dashboard', href: '/'},
         {label: 'Issues', href: '/issues'},
@@ -15,7 +22,15 @@ const NavBar = () => {
             {links.map(link => 
             <Link key={link.href} 
             href={link.href} 
-            className='text-zinc-500 hover:text-zinc-800 transition-colors'>{link.label}</Link>)}
+            // Likely more complicated solution:
+            // className={` ${link.href === currentPath ? 'text-zinc-900' :  'text-zinc-500'} hover:text-zinc-800 transition-colors`}>
+            className={classnames({
+                'text-zinc-900': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true
+            })}>
+                {link.label}
+                </Link>)}
         </ul>
     </nav>
   )
